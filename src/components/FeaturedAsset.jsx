eb0d4b61fcaf8ab3f7d095ada1ae2c4e022ede3a
Olivier Carignan <olivier@brunch.work>
2025-07-28 12:29:26 -0400

    preload videos

--- src/components/FeaturedAsset.jsx
+++ src/components/FeaturedAsset.jsx
@@ -1,9 +1,19 @@
 export const FeaturedAsset = ({ asset }) => {
-  if (asset.format === "jpg") {
+  if (asset.format === "jpg" || asset._modelApiKey === "image_asset") {
     return <img src={asset.url} alt={asset.alt} />;
   }
 
   if (asset.format === "mp4") {
-    return <video src={asset.url} alt={asset.alt} loop muted autoPlay playsInline />;
+    return (
+      <video
+        src={asset.url}
+        alt={asset.alt}
+        loop
+        muted
+        autoPlay
+        playsInline
+        preload="auto"
+      />
+    );
   }
 };
--- src/hooks/usePreloader.js
+++ src/hooks/usePreloader.js
@@ -1,20 +1,31 @@
-export const usePreloader = async (images, onProgress) => {
+export const usePreloader = async (assets, onProgress) => {
 
   let loaded = 0
-  const total = images.length
 
-  const loadImage = (src) => {
+  const loadImage = (asset) => {
     return new Promise((resolve) => {
-      const img = new Image()
-      img.onload = img.onerror = () => {
-        loaded++
-        onProgress(loaded.toString().padStart(2, "0"));
-        resolve()
+
+      if (asset.format === "jpg" || asset.format === "png") {
+        const img = new Image()
+        img.onload = img.onerror = () => {
+          loaded++
+          onProgress(loaded.toString().padStart(2, "0"));
+          resolve()
+        }
+        img.src = asset.url
       }
-      img.src = src
+
+      if (asset.format === "mp4") {
+        const video = document.createElement("video")
+        video.src = asset.url
+        video.addEventListener("loadeddata", () => {
+          loaded++
+          onProgress(loaded.toString().padStart(2, "0"));
+          resolve()
+        })
+      }
     })
   }
 
-  await Promise.all(images.map(loadImage))
-
+  await Promise.all(assets.map(loadImage))
 }
