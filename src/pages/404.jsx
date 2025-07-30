8c62c16f2bafffe954f8feede66b7bcaa1a76439
Olivier Carignan <olivier@brunch.work>
2025-07-30 11:03:08 -0400

    grid gap relative to screen size

---
+++ src/pages/404.jsx
@@ -0,0 +1,10 @@
+ import Monogram from "@/components/icons/Monogram";
+
+ export default function Page404() {
+
+  return (
+    <main className="page-404">
+      <Monogram />
+    </main>
+  )
+ }
